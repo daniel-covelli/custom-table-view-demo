9be73b5062775fe13f78df857d8d429be3b399ec
Daniel Covelli <danielcovelli@berkeley.edu>
2020-10-15 21:33:52 -0700

    Initial Commit

---
+++ CustomCellsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCellsDemo
+//
+//  Created by Daniel Covelli on 10/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
